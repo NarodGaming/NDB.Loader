34a7664e4fe2d68d4b258d2c928b833a52e7e3d9
NarodGaming <arron.faulkner@gmail.com>
2022-12-05 22:09:12 +0000

    Added ability for loader to load more items on start
    When NDB.Loader is constructed for the first time, it will read in config.loader.json to check and load in any additional wanted modules.
    
    This commit also allows the Loader to see the library type and version on 'liblist'.

--- NDB.Loader/NDB.Loader/Loader.cs
+++ NDB.Loader/NDB.Loader/Loader.cs
@@ -1,11 +1,47 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
 using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
 
 namespace NDB.Loader
 {
     public class Loader : ModuleBase<SocketCommandContext>
     {
         private static List<Loader_Structs.LibraryItem> LibraryItems = new();
+
+        private IConfiguration BuildConfig()
+        {
+            return new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("config.loader.json").Build();
+        }
+
+        public Loader() {
+            if (LibraryItems.Count == 0)
+            {
+                IConfiguration _loaderconfig = BuildConfig();
+
+                foreach (var item in _loaderconfig.AsEnumerable())
+                {
+                    Loader_Structs.LibraryItem newLibrary = new();
+                    newLibrary.LibraryName = item.Value;
+                    newLibrary.LibraryAssembly = Assembly.LoadFrom(item.Value);
+                    newLibrary.LibraryVersion = FileVersionInfo.GetVersionInfo(item.Value).ProductVersion;
+                    if (item.Key.Contains("lib")) { newLibrary.LibraryType = "Library"; } else if (item.Key.Contains("ser")) { newLibrary.LibraryType = "Service"; }
+                    LibraryItems.Add(newLibrary);
+                }
+                loadModulesConstructor();
+            }
+        }
+
+        private async void loadModulesConstructor()
+        {
+            for (int i = 0; i < LibraryItems.Count; i++)
+            {
+                var item = LibraryItems[i];
+                item.LibraryModules = await NDB_Main._commands.AddModulesAsync(item.LibraryAssembly, NDB_Main._services);
+            }
+        } 
         
         [Command("liblist")]
         [Summary("OWNER: Lists all currently loaded libraries")]
@@ -17,7 +53,7 @@ namespace NDB.Loader
                 String builderString = "";
                 foreach (Loader_Structs.LibraryItem library in LibraryItems)
                 {
-                    builderString += library.LibraryName + Environment.NewLine;
+                    builderString += $"{library.LibraryName} ({library.LibraryType} | v{library.LibraryVersion}) {Environment.NewLine}";
                 }
                 return ReplyAsync($"Currently loaded libraries: {Environment.NewLine}{builderString}");
             } else
@@ -101,7 +137,7 @@ namespace NDB.Loader
                     newLibrary.LibraryName = library;
                     newLibrary.LibraryAssembly = Assembly.LoadFrom(library);
                     newLibrary.LibraryModules = await NDB_Main._commands.AddModulesAsync(newLibrary.LibraryAssembly, NDB_Main._services);
-                    newLibrary.LibraryVersion = "1.0.0.0";
+                    newLibrary.LibraryVersion = FileVersionInfo.GetVersionInfo(library).ProductVersion;
                     newLibrary.LibraryType = "Library";
                     LibraryItems.Add(newLibrary);
                     await ReplyAsync($"Successfully loaded {library}!");
